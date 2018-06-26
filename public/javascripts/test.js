bf96783f7a76794c70c20d1d425d9e8840739f6d
zhangrh <zhangrh@lenovocloud.com>
2018-06-26 18:29:11 +0800

    js added

---
+++ public/javascripts/test.js
@@ -0,0 +1,20 @@
+//window.onload = function(){
+//    var pswdForm = document.getElementById("vcPassword");
+//    pswdForm.addEventListener("click" ,alert("kill la kill"));
+//}
+//function checkIp(){
+//    var formGroup = document.getElementById("vcpasswd");
+//    formGroup.setAttribute ("className", "has-success");
+//    formGroup.setAttribute ("className", "has-feedback");
+//}
+
+
+function addEsxiRow(row)
+{
+    var checkbox1 = document.createElement("input");
+    checkbox1.className = "form-check-input";
+    checkbox1.setAttribute("type", "checkbox")
+
+    var p1 = document.createElement("p");
+    p1.className
+}
