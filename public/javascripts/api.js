9e4c2dbdcf1b417d7a69de4b286a34d3b4e82973
zhangrh <zhangrh@lenovocloud.com>
2018-06-28 19:19:39 +0800

    add api

--- public/javascripts/api.js
+++ public/javascripts/api.js
@@ -1,3 +1,5 @@
+//1. vcenter  4
+//1.1 /v1/vcenter/validate [POST] 4
 $(document).ready(function(){
     console.log("ready for send requirest")
     $("#vcCheckButton").click(function(){
@@ -5,7 +7,6 @@ $(document).ready(function(){
       var vcUser = $("#vcUsername").val()
       var vcPwd = $("#vcPassword").val()
       var dataArr = '{"vcenterIP":"'+vcIp+'","vcenterUser":"'+vcUser+'","vcenterPwd":"'+vcPwd+'"}'
-      alert(dataArr);
       console.log(vcIp);
       console.log(vcUser);
       console.log(vcPwd);
@@ -20,3 +21,18 @@ $(document).ready(function(){
         });
     });
 );
+//2. datacenter 5
+//2.1 /v1/datacenter/list [GET] 5
+$(document).ready(function(){
+    console.log("ready for send requirest")
+    $("#vcCheckButton").click(function(){
+//3. cluster  6
+//3.1 /v1/cluster/list [GET]  6
+//4. host 7
+//4.1 /v1/host/validate [POST]  7
+//4.2 /v1/host/list [GET] 7
+//4.3 /v1/host/datastore/list [GET] 7
+//4.4 /v1/host/vSwitch/list [GET] 8
+//4.5 /v1/host/vnic/list [GET]  8
+//4.6 /v1/host/nic/list [GET] 
+
