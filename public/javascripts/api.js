a18e025c3110aac7be4e8855984bd26a167344f6
zhangrh <zhangrh@lenovocloud.com>
2018-06-29 11:35:37 +0800

    null

--- public/javascripts/api.js
+++ public/javascripts/api.js
@@ -1,33 +1,55 @@
 //1. vcenter  4
 //1.1 /v1/vcenter/validate [POST] 4
-$(document).ready(function(){
-    console.log("ready for send requirest")
-    $("#vcCheckButton").click(function(){
-      var vcIp = $("#vcIp").val()
-      var vcUser = $("#vcUsername").val()
-      var vcPwd = $("#vcPassword").val()
-      var dataArr = '{"vcenterIP":"'+vcIp+'","vcenterUser":"'+vcUser+'","vcenterPwd":"'+vcPwd+'"}'
-      console.log(vcIp);
-      console.log(vcUser);
-      console.log(vcPwd);
-        $.ajax({
-          type : "POST",
-          url : "http://172.16.21.206:17000/v1/actions/vcenter/validate",
-          dataType : "json",
-          data : dataArr,
-          success : function() {
-            console.log("success");
-          }
-        });
-    });
-);
+//$(document).ready(function(){
+//  console.log("ready for send requirest")
+//  $("#vcCheckButton").click(function(){
+//    var vcIp = $("#vcIp").val()
+//    var vcUser = $("#vcUsername").val()
+//    var vcPwd = $("#vcPassword").val()
+//    var dataArr = '{"vcenterIP":"'+vcIp+'","vcenterUser":"'+vcUser+'","vcenterPwd":"'+vcPwd+'"}'
+//    console.log(vcIp);
+//    console.log(vcUser);
+//    console.log(vcPwd);
+//      $.ajax({
+//        type : "POST",
+//        url : "http://172.16.21.206:17000/v1/actions/vcenter/validate",
+//        dataType : "jsonp",
+//        crossDomain: true,
+//        data : dataArr,
+//        success : function() {
+//          console.log("success");
+//        }
+//      });
+//  });
+//});
 //2. datacenter 5
 //2.1 /v1/datacenter/list [GET] 5
 $(document).ready(function(){
-    console.log("ready for send requirest")
-    $("#vcCheckButton").click(function(){
+  $("#dataCenterSelector").click(function(){
+    console.log("ready for list datacenter")
+    var vcIp = $("#vcIp").val()
+    var vcUser = $("#vcUsername").val()
+    var vcPwd = $("#vcPassword").val()
+    var dataArr = '{"vcenterIP":"'+vcIp+'","vcenterUser":"'+vcUser+'","vcenterPwd":"'+vcPwd+'"}'
+    console.log(vcIp);
+    console.log(vcUser);
+    console.log(vcPwd);
+      $.ajax({
+        type : "GET",
+        url : "http://172.16.21.206:17000/v1/actions/datacenter/list",
+        dataType : "jsonp",
+        crossDomain: true,
+        data : dataArr,
+        success : function() {
+          console.log("success");
+          alert(success);
+        }
+      });
+  });
+});
 //3. cluster  6
 //3.1 /v1/cluster/list [GET]  6
+
 //4. host 7
 //4.1 /v1/host/validate [POST]  7
 //4.2 /v1/host/list [GET] 7
--- public/javascripts/vcenterCheck.js
+++ public/javascripts/vcenterCheck.js
@@ -1,22 +1,21 @@
-$(document).ready(function(){
-    console.log("ready for send requirest")
-    $("#vcCheckButton").click(function(){
-      var vcIp = $("#vcIp").val()
-      var vcUser = $("#vcUsername").val()
-      var vcPwd = $("#vcPassword").val()
-      var dataArr = '{"vcenterIP":"'+vcIp+'","vcenterUser":"'+vcUser+'","vcenterPwd":"'+vcPwd+'"}'
-      alert(dataArr);
-      console.log(vcIp);
-      console.log(vcUser);
-      console.log(vcPwd);
-        $.ajax({
-          type : "POST",
-          url : "http://172.16.21.206:17000/v1/actions/vcenter/validate",
-          dataType : "json",
-          data : dataArr,
-          success : function() {
-            console.log("success");
-          }
-        });
-    });
-);
+//$(document).ready(function(){
+//    console.log("ready for send requirest")
+//    $("#vcCheckButton").click(function(){
+//      var vcIp = $("#vcIp").val()
+//      var vcUser = $("#vcUsername").val()
+//      var vcPwd = $("#vcPassword").val()
+//      var dataArr = '{"vcenterIP":"'+vcIp+'","vcenterUser":"'+vcUser+'","vcenterPwd":"'+vcPwd+'"}'
+//      console.log(vcIp);
+//      console.log(vcUser);
+//      console.log(vcPwd);
+//        $.ajax({
+//          type : "POST",
+//          url : "http://172.16.21.206:17000/v1/actions/vcenter/validate",
+//          dataType : "jsonp",
+//          data : dataArr,
+//          success : function() {
+//            console.log("success");
+//          }
+//        });
+//    });
+//});
